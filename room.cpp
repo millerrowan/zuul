2ac512a77aad92b7bbda1d76cb34e19c747f2aa3
U-BSD\412170 <412170@SHS-95W2NW3.beaverton.k12.or.us>
2023-11-17 12:29:09 -0800

    first commit

---
+++ room.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include <room.h> 
+//rooms
+
+using namespace std;
+
+room::room() {
+
+}
+
+
+//map<char*, room*>
+//"north", out
+//each room will have it's own map 
