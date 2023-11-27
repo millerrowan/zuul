089908b5c23bdef0dfacd09c0edf74f9c56de9fd
millerrowan <144379880+millerrowan@users.noreply.github.com>
2023-11-26 17:31:25 -0800

    commit 2

---
+++ zuul/zuul.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstring>
+#include "room.h"
+#include "item.h"
+#include <vector>
+//main code
+
+using namespace sd; 
+
+int main() {
+
+
+  
+  
+  
+  cout << "Welcome to the Davis Correctional Facility" << endl;
+  cout << "You were wrongly imprisoned here 4 years ago and now it's time to get out" << endl;
+  cout << "Stuck in your cell, you've devised an escape plan..." << endl;
+  cout << "Find the tools around the prison you need to escape and then take them to the perimeter fence" << endl;
+  cout << "Type 'help' if you need help." << endl;
+
+  //write room descriptions
+  char MC_Description[30];
+  strcpy(MC_Description, "You are in your cell");
+  
+  //creating rooms
+
+  room* myCell = new room();
+  room* cafeteria = new room();
+  room* wardenOffice = new room():
+  room* prisonYard = new room();
+  room* perimeter = new room();
+  room* guardStation = new room();
+  room* library = new room();
+  room* gym = new room();
+  room* kitchen = new room();
+  room* infirmary = new room();
+  room* cell2 = new room();
+  room* cell3 = new room();
+  room* bullpen = new room();
+  room* underground = new room();
+  room* forest = new room(); 
+
+
+  
+
+  while (stillPlaying == true) {
+       
+    
+    
+  }
+
+  return 0;
+}
